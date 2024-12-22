313bacebf59043518c04656cedb8bf2d9ed9cad7
Brylan Gannon <brylan.gannon112@gmail.com>
2024-12-22 15:03:43 -0700

    Add extra expectations to account type test

--- spec/requests/api/v1/account_types_spec.rb
+++ spec/requests/api/v1/account_types_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Api::V1::AccountTypes', type: :request do
   before do
     @account_types = create_list(:account_type, 11)
   end
+
   it 'GET /account_types' do
     get('/api/v1/account_types')
     expect(response).not_to be_nil
@@ -11,5 +12,14 @@ RSpec.describe 'Api::V1::AccountTypes', type: :request do
     json_response = JSON.parse(response.body)
     expect(json_response).to be_an(Array)
     expect(json_response).to be_all(Hash)
+
+    json_response.each do |account|
+      expect(account).to have_key(:name)
+      expect(account[:name]).to be_a(String)
+      expect(account).to have_key(:created_at)
+      expect(account[:created_at]).to be_a(String)
+      expect(account).to have_key(:updated_at)
+      expect(account[:updated_at]).to be_a(String)
+    end
   end
 end
